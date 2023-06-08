e794465bd11294bf51ba78aaa2825f9d9469bfe0
Saurabh Vishwakarma <100016387+SaurabhVishwakarma826@users.noreply.github.com>
2023-06-08 09:16:06 +0000

    added frontend react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello everyone we have successfully created our College Essential project using Django and React
+      </header>
+    </div>
+  );
+}
+
+export default App;
